eeccb29d0e9ef195f989c3ec980403f46a54bc56
Luke Conley <luke.sovern.conley@gmail.com>
2017-11-26 23:55:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay (musicians, instruments) {
 function johnLennonFacts (facts) {
 var i = 0;
   while (i < 4) {
-    facts[i]=`${facts[i]}!!!`;
+    facts[i]=facts[i]+'!!!';
       ++i;
   }
   return facts;
