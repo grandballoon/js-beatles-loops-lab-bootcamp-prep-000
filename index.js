f36c73e2d702788b949aa7ee23a93f68281ce0ec
Luke Conley <luke.sovern.conley@gmail.com>
2017-11-26 23:06:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ function theBeatlesPlay (musicians, instruments) {
   var whoPlays = [];
   for (let i = 0; i<3; i++) {
     whoPlays.push(`${musicians.i} plays ${instruments.i}`)
+    return whoPlays;
   }
 }
